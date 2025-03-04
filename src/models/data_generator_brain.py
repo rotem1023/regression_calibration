deea217dbcfc8bdc9dfddbf83e3f0e231e02ca14
Rotem Nizhar <rotemnizhar@dsiaudience01.local.biu.ac.il>
2025-03-04 21:26:01 +0200

    added brain generator

---
+++ src/models/data_generator_brain.py
@@ -0,0 +1,119 @@
+# camera-ready
+
+import torch
+import torch.utils.data
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+import numpy as np
+
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+import math
+import scipy.stats
+
+import pickle
+
+import cv2
+
+ROOT_DIR  = '/home/dsi/rotemnizhar/dev/regression_calibration/src/models/data'
+
+MAX_LABEL  = 362
+
+class BrainDatasetTrain(torch.utils.data.Dataset):
+    def __init__(self):
+        with open(f"{ROOT_DIR}/BrainTumourPixels/labels_train.pkl", "rb") as file: # (needed for python3)
+            self.labels = pickle.load(file)
+        with open(f"{ROOT_DIR}/BrainTumourPixels/images_train.pkl", "rb") as file: # (needed for python3)
+            self.imgs = pickle.load(file)
+
+        print (self.labels.shape)
+        print (self.imgs.shape)
+
+        self.num_examples = self.labels.shape[0]
+
+        print ("DatasetTrain - number of images: %d" % self.num_examples)
+        print (np.min(self.labels))
+        print (np.max(self.labels))
+        print (np.mean(self.labels))
+
+    def __getitem__(self, index):
+        label = self.labels[index]
+        img = self.imgs[index] # (shape: (64, 64, 3))
+
+        img = img/255.0
+        img = img - np.array([0.485, 0.456, 0.406])
+        img = img/np.array([0.229, 0.224, 0.225])
+        img = np.transpose(img, (2, 0, 1)) # (shape: (3, 64, 64))
+        img = img.astype(np.float32)
+
+        return (img, label/MAX_LABEL)
+
+    def __len__(self):
+        return self.num_examples
+
+class BrainDatasetVal(torch.utils.data.Dataset):
+    def __init__(self):
+        with open(f"{ROOT_DIR}/BrainTumourPixels/labels_val.pkl", "rb") as file: # (needed for python3)
+            self.labels = pickle.load(file)
+        with open(f"{ROOT_DIR}/BrainTumourPixels/images_val.pkl", "rb") as file: # (needed for python3)
+            self.imgs = pickle.load(file)
+
+        print (self.labels.shape)
+        print (self.imgs.shape)
+
+        self.num_examples = self.labels.shape[0]
+
+        print ("DatasetVal - number of images: %d" % self.num_examples)
+        print (np.min(self.labels))
+        print (np.max(self.labels))
+        print (np.mean(self.labels))
+
+    def __getitem__(self, index):
+        label = self.labels[index]
+        img = self.imgs[index] # (shape: (64, 64, 3))
+
+        img = img/255.0
+        img = img - np.array([0.485, 0.456, 0.406])
+        img = img/np.array([0.229, 0.224, 0.225])
+        img = np.transpose(img, (2, 0, 1)) # (shape: (3, 64, 64))
+        img = img.astype(np.float32)
+
+        return (img, label/MAX_LABEL)
+
+    def __len__(self):
+        return self.num_examples
+
+class BrainDatasetTest(torch.utils.data.Dataset):
+    def __init__(self):
+        with open(f"{ROOT_DIR}/BrainTumourPixels/labels_test.pkl", "rb") as file: # (needed for python3)
+            self.labels = pickle.load(file)
+        with open(f"{ROOT_DIR}/BrainTumourPixels/images_test.pkl", "rb") as file: # (needed for python3)
+            self.imgs = pickle.load(file)
+
+        print (self.labels.shape)
+        print (self.imgs.shape)
+
+        self.num_examples = self.labels.shape[0]
+
+        print ("DatasetTest - number of images: %d" % self.num_examples)
+        print (np.min(self.labels))
+        print (np.max(self.labels))
+        print (np.mean(self.labels))
+
+    def __getitem__(self, index):
+        label = self.labels[index]
+        img = self.imgs[index] # (shape: (64, 64, 3))
+
+        img = img/255.0
+        img = img - np.array([0.485, 0.456, 0.406])
+        img = img/np.array([0.229, 0.224, 0.225])
+        img = np.transpose(img, (2, 0, 1)) # (shape: (3, 64, 64))
+        img = img.astype(np.float32)
+
+        return (img, label/MAX_LABEL)
+
+    def __len__(self):
+        return self.num_examples
